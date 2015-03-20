e45fbc07095aed7d8f9c3ed3423d290efd8775c8
goodhanrry <hanrry@21cn.com>
2015-03-20 12:07:26 +0800

    first commit v11plus

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 7 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#7 SMP PREEMPT Wed Nov 26 12:30:04 CST 2014"
+#define LINUX_COMPILE_BY "hanrry"
+#define LINUX_COMPILE_HOST "hanrry-X201E"
+#define LINUX_COMPILER "gcc version 4.9.2 20141013 (prerelease) (crosstool-NG linaro-1.13.1+bzr2700 - Linaro GCC 2014.10) "
